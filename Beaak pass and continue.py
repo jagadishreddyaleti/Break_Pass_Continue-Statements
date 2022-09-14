52d149ad5a3225a7816de3bb434eacf55c526344
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-14 13:15:21 +0530

    Add files via upload

---
+++ Beaak pass and continue.py
@@ -0,0 +1,86 @@
+""" statements 14th sep 2022 """
+''' break '''  #  breaks when the condition becomes true
+# n=-1
+# while (n<20):
+#     n+=2
+#     if n>8:
+#         break
+#     print(n)   # to print oddd numbers
+
+# n=0
+# while (n<20):
+#     n+=2
+#     if n>8:
+#         break
+#     print(n) # to print even numbers
+
+# n=0
+# while (n<10):
+#     n+=1
+#     if n==4:
+#         break
+#     print(n)
+
+# n=0
+# while (n<20):
+#     n+=2
+#     if(n==10):
+#         break
+#     print(n)
+
+
+'''  continue '''   # skips the true value and prints false values o ststements
+
+# n=0
+# while (n<10):
+#     n+=1
+#     if n==6:
+#         continue
+#     print(n)   # skips 6 due to true statement
+
+# n=0
+# while (n<10):
+#     n+=1
+#     if n<5:
+#         continue
+#     print(n)  # skips 1 2 3 4 5 
+
+# n=0
+# while (n<10):
+#     n+=1
+#     if n>5:
+#         continue
+#     print(n)   # skips 6 7 8 9 10
+
+
+# n=0
+# while (n<10):
+#     n+=1
+#     if n>5:
+#         continue
+#     print(n) 
+
+# n=0
+# while (n<10):
+#     n+=1
+#     if n<=5:
+#         continue
+#     print(n) 
+
+
+''' pass '''  # to leave the print statement
+
+
+# if (11>2):
+#     pass
+# else:
+#     print(" false")
+
+
+# if (11<2):
+#     pass
+# else:
+#     print(" false")
+
+
+
